7189867d275915cd1dbf2d56d9d798ed90ffa78a
cjhdev <cam@cjh.id.au>
2018-11-10 13:16:30 +0000

    bump

--- lib/lib_signal.rb
+++ lib/lib_signal.rb
@@ -2,4 +2,9 @@ module LibSignal
 end
 
 require 'lib_signal/version'
+require 'lib_signal/key_pair'
+require 'lib_signal/persistence'
+require 'lib_signal/memory_backed'
+require 'lib_signal/ext_lib_signal'
+require 'lib_signal/ext_client'
 require 'lib_signal/client'
